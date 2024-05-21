2d0ba6654b2afece2108436437bae5c33e79634f
EmrecanKaracayir <developer@karacayir.com>
2024-05-21 07:03:39 +0300

    fixed: someStuff

--- src/core/login/LoginProvider.ts
+++ src/core/login/LoginProvider.ts
@@ -3,8 +3,6 @@ import { ProviderResponse } from "../../@types/responses";
 import { DbConstants } from "../../app/constants/DbConstants";
 import { IProvider } from "../../app/interfaces/IProvider";
 import { AccountModel } from "../../app/models/AccountModel";
-import { BtMacModel } from "../../app/models/BtMacModel";
-import { DocumentModel } from "../../app/models/DocumentModel";
 import { PairDocModel } from "../../app/models/PairDocModel";
 import { ResponseUtil } from "../../app/utils/ResponseUtil";
 
@@ -70,56 +68,10 @@ export class LoginProvider implements IProvider {
       throw error;
     }
   }
-
-  public async getBranchBtMacs(
-    branchId: number,
-  ): Promise<ProviderResponse<BtMacModel[] | null>> {
-    await DbConstants.POOL.query(DbConstants.BEGIN);
-    try {
-      const results: QueryResult = await DbConstants.POOL.query(
-        Queries.GET_BT_MACS$BID,
-        [branchId],
-      );
-      const record: unknown[] = results.rows;
-      if (!record) {
-        return await ResponseUtil.providerResponse(null);
-      }
-      return await ResponseUtil.providerResponse(
-        BtMacModel.fromRecords(record),
-      );
-    } catch (error) {
-      await DbConstants.POOL.query(DbConstants.ROLLBACK);
-      throw error;
-    }
-  }
-
-  public async getDocuments(
-    branchId: number,
-  ): Promise<ProviderResponse<DocumentModel[] | null>> {
-    await DbConstants.POOL.query(DbConstants.BEGIN);
-    try {
-      const results: QueryResult = await DbConstants.POOL.query(
-        Queries.GET_DOCUMENTS$BID,
-        [branchId],
-      );
-      const records: unknown[] = results.rows;
-      if (!records) {
-        return await ResponseUtil.providerResponse(null);
-      }
-      return await ResponseUtil.providerResponse(
-        DocumentModel.fromRecords(records),
-      );
-    } catch (error) {
-      await DbConstants.POOL.query(DbConstants.ROLLBACK);
-      throw error;
-    }
-  }
 }
 
 enum Queries {
   GET_ACCOUNT$UNAME = `SELECT * FROM "Account" WHERE "username" = $1;`,
   ASSIGN_SHARED_OTP$SOTP_$AID = `UPDATE "Account" SET "sharedOtp" = $1 WHERE "accountId" = $2;`,
   GET_PAIR_DOC$AID = `SELECT * FROM "PairDocView" WHERE "firstAccountId" = $1 OR "secondAccountId" = $1;`,
-  GET_BT_MACS$BID = `SELECT "btMac" FROM "BtMacsView" WHERE "branchId" = $1;`,
-  GET_DOCUMENTS$BID = `SELECT "content" FROM "Document" WHERE "branchId" = $1;`,
 }
